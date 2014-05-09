a43818a356024431711060755bbbd81b057ab728
Felipe Contreras <felipe.contreras@gmail.com>
2014-05-09 08:13:44 -0500

    test: use git.git author and committer
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- test/test-lib.sh
+++ test/test-lib.sh
@@ -3,3 +3,10 @@
 . ./sharness.sh
 
 test_set_prereq PYTHON
+
+GIT_AUTHOR_EMAIL=author@example.com
+GIT_AUTHOR_NAME='A U Thor'
+GIT_COMMITTER_EMAIL=committer@example.com
+GIT_COMMITTER_NAME='C O Mitter'
+export GIT_AUTHOR_EMAIL GIT_AUTHOR_NAME
+export GIT_COMMITTER_EMAIL GIT_COMMITTER_NAME
