9a80e68234f7db339606d3d06127645256d4aee8
Felipe Contreras <felipe.contreras@gmail.com>
2025-08-01 19:20:15 -0600

    t: update to sharness 1.2.1

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -17,7 +17,63 @@ export GIT_COMMITTER_EMAIL GIT_COMMITTER_NAME
 
 unset XDG_CONFIG_HOME
 
+test_set_prereq() {
+	satisfied_prereq="$satisfied_prereq$1 "
+}
+satisfied_prereq=" "
+
 if [[ $(uname -s) = MSYS* ]]; then
 	test_set_prereq WIN
 	export TEST_CMP='diff --strip-trailing-cr -u'
 fi
+
+test_cmp() {
+	${TEST_CMP:-diff -u} "$@"
+}
+
+test_when_finished() {
+	test_cleanup="{ $*
+		} && (exit \"\$eval_ret\"); eval_ret=\$?; $test_cleanup"
+}
+
+test_expect_code() {
+	want_code=$1
+	shift
+	"$@"
+	exit_code=$?
+	if test "$exit_code" = "$want_code"; then
+		return 0
+	fi
+
+	echo >&2 "test_expect_code: command exited with $exit_code, we wanted $want_code $*"
+	return 1
+}
+
+test_have_prereq() {
+	prerequisite=$1
+
+	case "$prerequisite" in
+	!*)
+		negative_prereq=t
+		prerequisite=${prerequisite#!}
+		;;
+	*)
+		negative_prereq=
+	esac
+
+	case "$satisfied_prereq" in
+	*" $prerequisite "*)
+		satisfied_this_prereq=t
+		;;
+	*)
+		satisfied_this_prereq=
+	esac
+
+	case "$satisfied_this_prereq,$negative_prereq" in
+	t,|,t)
+		return 0
+		;;
+	esac
+
+	return 1
+}
